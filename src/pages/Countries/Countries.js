5fd88bdc2f9e86cc8aa7e19127b339f0b7fea769
niasalgado <salgado.nia@gmail.com>
2023-06-10 13:30:13 -0400

    wip: adds search functionality, updates favicon

--- src/pages/Countries/Countries.js
+++ src/pages/Countries/Countries.js
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import CountryCard from '../../components/CountryCard/CountryCard';
 import NavBar from '../../components/NavBar/NavBar';
-
 import './countries.css';
 
 export default function Countries( {setPlanCountry, planCountry} ) {
@@ -11,7 +10,7 @@ export default function Countries( {setPlanCountry, planCountry} ) {
             const response = await fetch('https://restcountries.com/v3.1/all');
             const data = await response.json();
 
-            setCountries(data.slice(0,10));
+            setCountries(data.slice(10,20));
         } catch (error) {
             console.log(error)
         }
@@ -21,10 +20,28 @@ export default function Countries( {setPlanCountry, planCountry} ) {
         fetchCountries();
     }, []);
 
+    const regions = ['Africa', 'Europe', 'Asia', 'Americas', 'Oceania', 'Antarctic']
+
     return (
         <>
         <NavBar />
         <div>Countries</div>
+
+        <div id='searchContainer'>
+            <form>
+                <input type='text' id='searchbar' placeholder='Search for a country' required></input>
+            </form>
+            <form>
+                <select id='filter-by-region'>
+                    {regions.map((region) => (
+                        <option value={region}>
+                            {region}
+                        </option>
+                    ))}
+                </select>
+            </form>
+        </div>
+
         <div className="cardGrid">
             {countries.map((country) => (
                 <CountryCard key={country.name.common} {...country} setPlanCountry={setPlanCountry} planCountry={planCountry} />
