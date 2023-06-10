eeff05a66a3af2f7bd51eec0ed5fe7244e49367c
niasalgado <salgado.nia@gmail.com>
2023-06-10 19:32:01 -0400

    wip: search functionality

--- src/pages/Countries/Countries.js
+++ src/pages/Countries/Countries.js
@@ -21,19 +21,21 @@ export default function Countries( {setPlanCountry, planCountry} ) {
     }, []);
 
     const regions = ['Africa', 'Europe', 'Asia', 'Americas', 'Oceania', 'Antarctic'];
-    const [search, setSearch] = useState();
+    const [search, setSearch] = useState('');
+    console.log('search text: ', search)
 
     async function searchCountry() {
         try {
             const response = await fetch(`https://restcountries.com/v3.1/name/${search}`)
             const data = await response.json();
-            setSearch(data)
+            setCountries(data)
         } catch (error) {
             console.log(error)
         }
     }
 
     function handleSearch(evt) {
+        evt.preventDefault()
         searchCountry()
     }
 
@@ -42,8 +44,15 @@ export default function Countries( {setPlanCountry, planCountry} ) {
         <>
         <NavBar />
         <div id='searchContainer'>
-            <form onSubmit={handleSearch()}>
-                <input type='text' id='searchbar' placeholder='Search for a country' required></input>
+            <form onSubmit={handleSearch}>
+                <input 
+                    type='text' 
+                    id='searchbar' 
+                    value={search} 
+                    onChange={(evt) => setSearch(evt.target.value)}
+                    placeholder='Search for a country' 
+                    required>    
+                </input>
             </form>
             <form>
                 <select id='region-selector'>
