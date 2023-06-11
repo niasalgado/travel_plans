a7a2b5e16a3ff5e375136f48a09fbea1ff3a585e
niasalgado <salgado.nia@gmail.com>
2023-06-11 18:48:22 -0400

    wip: styling app

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import NavBar from '../../components/NavBar/NavBar'
+import './home.css'
 
 export default function Home() {
   return (
     <>
         <NavBar />
-        <h1>Travel Plans</h1>
+        <div id='title'>
+          <h1>Travel Plans</h1>
+        </div>
     </>
   )
 }
